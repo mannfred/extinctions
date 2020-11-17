39328279e56e4dac20b372f42aa0da8270cd14c9
mannfred <mannfredboehm@gmail.com>
2020-11-17 12:06:12 -0800

    constrain SEUX DEs to 1800-2012

--- Rscripts/cronk_method_birds.R
+++ Rscripts/cronk_method_birds.R
@@ -1,58 +1,81 @@
 library(here)
 library(tidyverse)
 
+
+# data import
 birds2 <- 
   read.csv(here('Data/figure2_data.csv')) %>% 
   slice(7:12) # capture data from 1800 - present
 
-plot(birds2)
 
-# first model: extinctions = 0.462(year) - 792.454
-# where B_1 = -792.454
+# ----------------------------
+# linear model with no DEs
+
+# extinctions = mu*year + b
+# extinctions = 0.462*year - 792
 model1 <- 
   lm(birds2$extinct ~ birds2$date)
 
+mu1 <- as.numeric(model1$coefficients[2])
+b1  <- as.numeric(model1$coefficients[1]) 
 
-# extrapolate year when extinctions = 0
-# year = 1715
-792.454/0.462
 
-# now force extinctions = 0 for year 1500
-# 0 = 0.462(1500) - B_2
-# B_2 = -693
-# B_1 - B_2 = dB
-# dB = 792.454 - 693 = 99.454
-0.462*1500 
+# ---------------------------
+# add DEs from SEUX analysis
+# and spread out evenly from 1800-2012
 
-# with the adjusted model, how many extinctions at year 2012?
-# extinctions = 0.462(2012) - 693
-# total extinctions = 236
-# observed extinctions = 134
-# dark extinctions = 236 - 134 = 102
-0.462*2012 - 693
+# add DE rate to mu1
+mu2 <- mu1 + (55/(2012-1800))
 
-# now modify data to give 0 extinct at year 1500
-birds3 <-
+# solve for intercept b2: 
+# we know that by 2012 there should
+# be 55 more extinctions than the OG dataset
+# b2 = -1262.557
+(max(birds2$extinct) + 55) - (mu2*2012)
+
+f_seuxDE <- function(x) (mu2*x) - 1262.557
+
+# create dataset from new model
+birds_seuxDEs <-
   birds2 %>% 
-  mutate(extinct = extinct + 102)
+  mutate(extinct = f_seuxDE(birds2$date))
 
+
+# ---------------------------
+# extrapolate so that there are 
+# zero extinctions in year 1500
+
+# b3 = -(0.721*1500) = -1081.5
+f_pretax <- function(x) (mu2*x) - 1081.5
+
+# zero extinctions at year 1500
+f_pretax(1500)
+
+
+# -------------------------
+# create new data points to compare the SEUX and pretax models
+
+# for pretaxonomic DEs
+# calculate extinctions for dates of interest
+birds_pretax <-
+  birds2 %>% 
+  mutate(extinct = f_pretax(birds2$date))
+
+# -------------------------
 # plotting
+
 plot_data <-
-  full_join(birds2, birds3) %>% 
-  mutate(type = c(rep('no DEs', 6), rep('DEs added', 6)))
-
-
-mycolours <- c(rep("#CC79A7", 6), rep("#56B4E9", 6))
-
+  rbind(birds2, birds_seuxDEs, birds_pretax) %>% 
+  mutate(model = c(rep('no DEs', 6), rep('SEUX DEs', 6), rep('SEUX DEs + Cronk Method', 6)))
 
 ggplot(plot_data) +
-  aes(x = date, y = extinct, colour = type) +
+  aes(x = date, y = extinct, colour = model) +
   geom_point(size = 3) + 
   expand_limits(x = 1500) +
   theme_bw() + 
   # geom_smooth(method = 'lm') +
   stat_smooth(method = 'lm', fullrange = TRUE)
-
+# -----------------------------------------
 # from SEUX we know there were 55 dark extinctions
 # we need to add them while keeping extinctions at 1500 = 0
 # so let's add them by increasing extinction rate
@@ -86,6 +109,10 @@ ggplot(plot_data) +
   theme_bw() + 
   # geom_smooth(method = 'lm') +
   stat_smooth(method = 'lm', fullrange = TRUE)
+
+
+
+
 
 # ------------------------------------------------------
 # let's calculate the slope ("extinction rate") manually
